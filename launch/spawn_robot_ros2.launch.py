f9ae1643dd4ca85082b8646e5e5f29254a4b09ca
kunj <kgolwala@umd.edu>
2024-10-26 22:59:57 -0400

    Proportional controller working

--- launch/spawn_robot_ros2.launch.py
+++ launch/spawn_robot_ros2.launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
 
     # Position and orientation
     # [X, Y, Z]
-    position = [1.0, 0.0, 0.5]
+    position = [0.0, 0.0, 0.2]
     # [Roll, Pitch, Yaw]
     orientation = [0.0, 0.0, 0.0]
     # Base Name or robot
